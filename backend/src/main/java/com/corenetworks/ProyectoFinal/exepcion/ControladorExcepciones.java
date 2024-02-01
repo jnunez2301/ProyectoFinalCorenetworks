dfa8f729ab768fe981f66425ebb99fe78bb48aa7
JoaoCzz <joaoczz0205@gmail.com>
2024-02-01 18:31:36 +0100

    Para mandar mensaje en el JSON

--- backend/src/main/java/com/corenetworks/ProyectoFinal/exepcion/ControladorExcepciones.java
+++ backend/src/main/java/com/corenetworks/ProyectoFinal/exepcion/ControladorExcepciones.java
@@ -3,12 +3,14 @@ package com.corenetworks.ProyectoFinal.exepcion;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 @RestController
 @ControllerAdvice
 public class ControladorExcepciones extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ExcepcionError.class)
     public ResponseEntity<ExcepcionDetalles> buscadorErrores(ExcepcionDetalles ed, WebRequest peticion){
         ExcepcionDetalles e= new ExcepcionDetalles(ed.getFecha(), ed.getMensaje(), peticion.getDescription(false));
         return new ResponseEntity<>(e, HttpStatus.NOT_FOUND);
