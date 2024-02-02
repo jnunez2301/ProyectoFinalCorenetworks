556a09901c1658237e71e965ce043caec92221f7
JoaoCzz <joaoczz0205@gmail.com>
2024-02-02 20:14:14 +0100

    service en mensaje arreglado

--- backend/src/main/java/com/corenetworks/ProyectoFinal/servicio/IMensajeServicioImpl.java
+++ backend/src/main/java/com/corenetworks/ProyectoFinal/servicio/IMensajeServicioImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.List;
-
+@Service
 public class IMensajeServicioImpl extends ICRUDimpl <Mensaje, Integer> implements IMensajeServicio {
     @Autowired
     private IMensajeRepositorio repositorio;
