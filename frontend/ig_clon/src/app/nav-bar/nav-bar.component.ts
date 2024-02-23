857390756859ec35d19ac669e59cb9d99591ed4d
jnunez2301 <jmoises.n23@gmail.com>
2024-02-23 20:19:02 +0100

    NavBar lista

--- frontend/ig_clon/src/app/nav-bar/nav-bar.component.ts
+++ frontend/ig_clon/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-
+import { MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
