a33c8fd6370edaec9006d40005060f42a8daf5c8
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-11 19:12:14 +0100

    Mensajes añadidos

--- frontend/ig_clon/src/app/mensajes/mensajes.component.ts
+++ frontend/ig_clon/src/app/mensajes/mensajes.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../_modelo/Usuario';
+import { UsuarioService } from '../_servicio/usuario.service';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionAddCircleOutline, ionChatboxEllipsesOutline, ionHappyOutline } from '@ng-icons/ionicons';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-mensajes',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, FormsModule],
   templateUrl: './mensajes.component.html',
-  styleUrl: './mensajes.component.css'
+  styleUrl: './mensajes.component.css',
+  viewProviders: [provideIcons({ ionAddCircleOutline, ionHappyOutline, ionChatboxEllipsesOutline })],
 })
-export class MensajesComponent {
+export class MensajesComponent implements OnInit {
+  public listaUsuarios$!: Usuario[];
+  mensaje:string = '';
+  errorMsg:string = '';
 
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.usuarioService.getUsuarios().subscribe((data) => {
+      this.listaUsuarios$ = data;
+    });
+  }
+
+  enviarMensaje():void{
+    if(this.mensaje.length < 2){
+      this.errorMsg = 'La longitud del mensaje debe ser superior a 2ch'
+    }
+    console.log(this.mensaje);
+    console.log('mensaje enviado');
+    this.mensaje = '';
+    
+    
+  }
 }
