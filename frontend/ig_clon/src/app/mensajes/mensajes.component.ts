5debcace60bb582f8496b79dd458f3acc2b54f11
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-11 19:23:03 +0100

    EmojiPicker add

--- frontend/ig_clon/src/app/mensajes/mensajes.component.ts
+++ frontend/ig_clon/src/app/mensajes/mensajes.component.ts
@@ -2,20 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../_modelo/Usuario';
 import { UsuarioService } from '../_servicio/usuario.service';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { ionAddCircleOutline, ionChatboxEllipsesOutline, ionHappyOutline } from '@ng-icons/ionicons';
+import {
+  ionAddCircleOutline,
+  ionChatboxEllipsesOutline,
+  ionHappyOutline,
+} from '@ng-icons/ionicons';
 import { FormsModule } from '@angular/forms';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 @Component({
   selector: 'app-mensajes',
   standalone: true,
-  imports: [NgIconComponent, FormsModule],
+  imports: [NgIconComponent, FormsModule, PickerComponent],
   templateUrl: './mensajes.component.html',
   styleUrl: './mensajes.component.css',
-  viewProviders: [provideIcons({ ionAddCircleOutline, ionHappyOutline, ionChatboxEllipsesOutline })],
+  viewProviders: [
+    provideIcons({
+      ionAddCircleOutline,
+      ionHappyOutline,
+      ionChatboxEllipsesOutline,
+    }),
+  ],
 })
 export class MensajesComponent implements OnInit {
   public listaUsuarios$!: Usuario[];
-  mensaje:string = '';
-  errorMsg:string = '';
+
+  /*  EMOJI PICKER  */
+
+  mensaje: string = '';
+  errorMsg: string = '';
 
   constructor(private usuarioService: UsuarioService) {}
 
@@ -25,14 +39,12 @@ export class MensajesComponent implements OnInit {
     });
   }
 
-  enviarMensaje():void{
-    if(this.mensaje.length < 2){
-      this.errorMsg = 'La longitud del mensaje debe ser superior a 2ch'
+  enviarMensaje(): void {
+    if (this.mensaje.length < 2) {
+      this.errorMsg = 'La longitud del mensaje debe ser superior a 2ch';
     }
     console.log(this.mensaje);
     console.log('mensaje enviado');
     this.mensaje = '';
-    
-    
   }
 }
