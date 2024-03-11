dea1465ac41fbaccba414e47e4279559a113757f
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-03-11 19:50:08 +0100

    Fix en peticiones

--- frontend/ig_clon/src/app/_servicio/perfil.service.ts
+++ frontend/ig_clon/src/app/_servicio/perfil.service.ts
@@ -4,31 +4,26 @@ import { Observable } from 'rxjs';
 import { Perfilusuario } from '../_modelo/perfilusuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PerfilService {
-  // confirmar url 
-  url:string = "http:/localhost:3000/api/perfil"
-  constructor(private http: HttpClient) { }
+  // confirmar url
+  baseURL: string = 'http://localhost:3000/api/usuarios';
+  constructor(private http: HttpClient) {}
 
-  obtenerPerfil(id: number): Observable <Perfilusuario> {
-  return this.http.get<Perfilusuario>(`${this.url}/${id}`);
+  obtenerPerfil(id: number): Observable<Perfilusuario> {    
+    return this.http.get<Perfilusuario>(`${this.baseURL}/${id}`);
   }
 
-  actualizarPerfil(p:Perfilusuario): Observable <Perfilusuario>  {
-    return this.http.put<Perfilusuario>(`${this.url}/${p.idPerfil}`,p);
-
+  actualizarPerfil(p: Perfilusuario): Observable<Perfilusuario> {
+    return this.http.put<Perfilusuario>(`${this.baseURL}/${p.idPerfil}`, p);
   }
-    
+
   eliminarPerfil(id: number) {
-    return this.http.delete (`${this.url}/${id}`);
-
-
+    return this.http.delete(`${this.baseURL}/${id}`);
   }
 
-  altaPerfil(p:Perfilusuario): Observable <Perfilusuario>  {
-    return this.http.post<Perfilusuario>(`${this.url}/${p.idPerfil}`,p);
-
+  altaPerfil(p: Perfilusuario): Observable<Perfilusuario> {
+    return this.http.post<Perfilusuario>(`${this.baseURL}/${p.idPerfil}`, p);
   }
-  
 }
--- frontend/ig_clon/src/app/editarperfilformulario/editarperfilformulario.component.ts
+++ frontend/ig_clon/src/app/editarperfilformulario/editarperfilformulario.component.ts
@@ -1,33 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Perfilusuario } from '../_modelo/perfilusuario';
-import { PerfilService } from '../perfil.service';
+import { PerfilService } from '../_servicio/perfil.service';
 
 @Component({
   selector: 'app-editarperfilformulario',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './editarperfilformulario.component.html',
-  styleUrl: './editarperfilformulario.component.css'
+  styleUrl: './editarperfilformulario.component.css',
 })
 export class EditarperfilformularioComponent implements OnInit {
+  pu: Perfilusuario = {
+    idPerfil: 1,
+    nombreUsuario: '',
+    contrasena: '',
+    correo: '',
+    fotoPerfil: '',
+  };
   ngOnInit(): void {
-    this.servicio.obtenerPerfil(1).subscribe(data => {this.pu=data
-    })}
-  pu: Perfilusuario= {
-    "idPerfil":1,
-    "nombreUsuario":"",
-    "contrasena": "",
-    "correo":"",
-    "fotoPerfil":"",
+    this.servicio.obtenerPerfil(1).subscribe((data) => {
+      this.pu = data;      
+    });
   }
-  
-  constructor(private servicio:PerfilService) {
 
-   }
-  
+  constructor(private servicio: PerfilService) {}
+
   guardarCambios() {
-   this.servicio.actualizarPerfil(pu).subscribe(()=>console.log("se ha cambiado"));
-   
+    this.servicio
+      .actualizarPerfil(this.pu)
+      .subscribe(() => console.log('se ha cambiado'));
   }
 }
